7ef75819d24fe06a70a215285c656248426fd80f
andreyyuryev <yuryev@yandex.ru>
2021-06-07 17:14:43 +0300

    odata

--- Odata/sample_odata.py
+++ Odata/sample_odata.py
@@ -3,9 +3,15 @@ import requests
 
 
 # SERVICE_URL = 'http://erpcixpl01:8001/sap/opu/odata/sap/ZCS_EBGCP_SRV/'
-SERVICE_URL = 'https://psl-e.one-erp.telekom.de/sap/opu/odata/sap/ZCS_EBGCP_SRV/$metadata?sap-client=400&sap-language=EN$format=xml'
+SERVICE_URL = 'https://psl-e.one-erp.telekom.de/sap/opu/odata/sap/ZCS_EBGCP_SRV/$metadata?sap-client=300&sap-language=EN$format=xml'
 
 session = requests.Session()
-session.auth = ('44544331', 'MyPassword')
+session.auth = ('44544331', 'bFdn2RY8A!5u')
 
 theservice = pyodata.Client(SERVICE_URL, session)
+
+order = theservice.entity_sets.PMOrderSet.get_entity('000300229310')
+
+# print(order._entity_key._proprties.items())
+# print(order._entity_key.items())
+
