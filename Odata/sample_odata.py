18355dc70145e2aaaf0563d05e554359a2caedf1
andreyyuryev <yuryev@yandex.ru>
2021-06-08 13:44:39 +0300

    odata

--- Odata/sample_odata.py
+++ Odata/sample_odata.py
@@ -1,17 +1,39 @@
 import pyodata
 import requests
+from pyodata.v2.service import GetEntitySetFilter as sef
 
 
 # SERVICE_URL = 'http://erpcixpl01:8001/sap/opu/odata/sap/ZCS_EBGCP_SRV/'
-SERVICE_URL = 'https://psl-e.one-erp.telekom.de/sap/opu/odata/sap/ZCS_EBGCP_SRV/$metadata?sap-client=300&sap-language=EN$format=xml'
+SERVICE_URL = 'https://psl-e.one-erp.telekom.de//sap/opu/odata/SAP/ZPSL_GWSAMPLE_BASIC_SRV/BusinessPartnerSet?$format=json&sap-client=400&sap-language=EN'
 
 session = requests.Session()
-session.auth = ('44544331', 'bFdn2RY8A!5u')
+session.auth = ('44544331', 'Fhm9Z2478p!EW')
 
-theservice = pyodata.Client(SERVICE_URL, session)
+services = pyodata.Client(SERVICE_URL, session)
 
-order = theservice.entity_sets.PMOrderSet.get_entity('000300229310')
 
+
+bp_request = services.entity_sets.BusinessPartnerSet.get_entities()
+#bp_request = bp_request.filter("Address eq 'Waldorf'")
+#print(bp_request)
+for itm in bp_request.execute():
+    print(itm)
+# bp_request = bp_request.filter(sef.and_(bp_request.Address['City'] == 'Waldorf', bp_request.
+#                                         Address['PostalCode'] == '69190'))
+# for bp in bp_request.execute():
+#     print(bp.PostalCode)
+
+# print(partners.Address)
+# r = partners.Address
+# for itm in r['City']:
+#     print(itm)
+# partner = partners.select('City, PostalCode').execute()
+# for item in partner.execute():
+#     print(item)
+# order = services.entity_sets.PMOrderSet.get_entity('000300229310')
+# for service in services.entity_sets._entity_sets:
+#     print(service)
 # print(order._entity_key._proprties.items())
 # print(order._entity_key.items())
+print('test')
 
