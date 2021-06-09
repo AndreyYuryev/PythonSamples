1605a7efaf2207ba5ccd4ddde6aecfc51298d949
andreyyuryev <yuryev@yandex.ru>
2021-06-09 15:53:29 +0300

    odata

--- Odata/sap_gwsample.py
+++ Odata/sap_gwsample.py
@@ -1,11 +1,11 @@
 import pyodata
-import requests
+from requests import Request, Session
 from pyodata.v2.model import PolicyFatal, PolicyWarning, PolicyIgnore, ParserError, Config
 
 
 SERVICE_URL = 'http://psl-e.one-erp.telekom.de/sap/opu/odata/SAP/ZPSL_GWSAMPLE_BASIC_SRV/'
 
-session = requests.Session()
+session = Session()
 # 400
 session.auth = ('44544331', 'Fhm9Z2478p!EW')
 # 200
@@ -18,12 +18,11 @@ namespaces = {
 }
 custom_config = Config(xml_namespaces=namespaces,
                        default_error_policy=PolicyFatal(),
-                       custom_error_policies={
-         ParserError.ANNOTATION: PolicyWarning(),
-         ParserError.ASSOCIATION: PolicyIgnore()
-    })
+                       custom_error_policies={ParserError.ANNOTATION: PolicyWarning(),
+                                              ParserError.ASSOCIATION: PolicyIgnore()})
 
-services = pyodata.Client(url=SERVICE_URL,connection=session, config=custom_config)
+services = pyodata.Client(url=SERVICE_URL, connection=session, config=custom_config)
 bp_request = services.entity_sets.BusinessPartnerSet.get_entities()
-for item in bp_request.execute():
-    print(item.BusinessPartnerID, item.CompanyName)
+for item in bp_request.select('BusinessPartnerID,CompanyName').execute():
+    # print(item.BusinessPartnerID, item.CompanyName)
+    print(item.EmailAddress)
